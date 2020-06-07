d003525f24769e7576a2ab2b2c0acab9f0917b12
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-07 18:49:47 +0530

    Set the default greeting to hello

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,5 @@
 const helloList = ["Hello", "Hola"];
 const helloContainer = document.getElementById('hello');
-helloContainer.innerHTML = `<p>Hello!</p>`;
 
 setInterval(() => {
     let randIndex = Math.ceil(Math.random() * helloList.length) - 1;

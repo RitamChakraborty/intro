c0d8d4c71a6a8b34dd44ec3e8352e24c942a1663
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-07 19:02:47 +0530

    Add hello in every language started with letter a, b, c

---
+++ public/script/hello_list.js
@@ -0,0 +1,20 @@
+const helloList = [
+    "Përshëndetje",
+    "እው ሰላም ነው",
+    "مرحبا",
+    "Բարեւ Ձեզ",
+    "Salam",
+    "হ্যালো",
+    "Kaixo",
+    "добры дзень",
+    "zdravo",
+    "Здравейте",
+    "ဟယ်လို",
+    "Hola",
+    "Kumusta",
+    "你好",
+    "你好",
+    "Bonghjornu",
+    "zdravo",
+    "Ahoj"
+];

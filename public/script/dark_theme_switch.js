65780e15ca0ae616cb5d3ac11ffe99309ae57aee
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-09 11:24:00 +0530

    Add theme change

--- public/script/dark_theme_switch.js
+++ public/script/dark_theme_switch.js
@@ -2,13 +2,18 @@ let lightTheme = true;
 
 function toggleTheme() {
     const themeSwitch = document.getElementById('brightness');
+    const everything = document.querySelector("*");
 
     if (lightTheme) {
         themeSwitch.src = "./asset/dark.svg";
         lightTheme = false;
+        everything.style.color = "whitesmoke";
+        everything.style.backgroundColor = "darkslategray";
     } else {
         themeSwitch.src = "./asset/light.svg";
-        lightTheme = true
+        lightTheme = true;
+        everything.style.color = "darkslategray";
+        everything.style.backgroundColor = "whitesmoke";
     }
 }
 
--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,12 @@
 import {toggleTheme} from "./dark_theme_switch.js";
 import {helloList} from "./hello_list.js";
 
+// Set primary color and background color
+const everything = document.querySelector("*");
+everything.style.color = "darkslategray";
+everything.style.backgroundColor = "whitesmoke";
+
+// Change theme
 const themeSwitch = document.getElementById('brightness');
 themeSwitch.addEventListener('click', toggleTheme);
 
